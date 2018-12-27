248fc780b21e4b5ad0bac5876dc07afe29d69f92
Ado Moshe <adoran.moshe@gmail.com>
2018-12-27 11:51:02 -0800

    revisting project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,115 +1,84 @@
-    // List of Tarantino movies used in the game
-var movieList = ["Pulp Fiction", "Inglorious Basterds", "Reservoir Dogs", "The Hateful Eight", "Kill Bill", "Jackie Brown", "Django Unchained"],
+'use strict';
 
-    // Picks a random movie from the movieList array
-    moviePick = movieList[Math.floor(Math.random() * movieList.length)],
+// List of Tarantino movies used in the game
+const movieList = [
+  'Pulp Fiction',
+  'Inglorious Basterds',
+  'Reservoir Dogs',
+  'The Hateful Eight',
+  'Kill Bill',
+  'Jackie Brown',
+  'Django Unchained'
+];
 
-    // Turns the movie picked into lower case to interact better wtih the game
-    moviePickLowerCase = moviePick.toLowerCase(),
+const wins = 0,
+  losses = 0;
 
-    // Determines how many characters moviePick has
-    numberOfLetters = moviePickLowerCase.length,
+let spaceCounter = 0;
 
-    // Keeping track of wins
-    wins = 0,
+const start = () => {
+  const collectPositions = [],
+    moviePicklow = randomMovie(),
+    moviePick = moviePicklow.toUpperCase();
 
-    // Keeping track of losses
-    losses = 0,
+  console.log(moviePick);
 
-    // Keeping track of guesses remaining (also used to reset the game when = 0)
+  let userGuess = '',
     remainingGuesses = 5,
+    wordBeingGuessed = '',
+    guessedLetters = '';
 
-    // Forms the word being guessed by the player in an array
-    wordBeingGuessed = "",
+  for (let i = 0; i < moviePick.length; i++) {
+    wordBeingGuessed += '_ ';
+  }
+  updateHTML(wordBeingGuessed, guessedLetters, remainingGuesses);
 
-    // Declares variable guessedLetters as an array that we will populate with letters the player guessed but were not in the moviePick
-    guessedLetters = [];
+  document.addEventListener('keyup', event => {
+    if (event.keyCode < 65 || event.keyCode > 90) {
+      return alert(`${event.key} is an invalid key`);
+    }
+    const userGuess = event.key.toUpperCase();
+    console.log(`User Guess ${userGuess}`);
 
-console.log(moviePick)
-console.log(numberOfLetters)
-console.log(wins)
-console.log(losses)
+    if (guessedLetters.indexOf(userGuess) !== -1) {
+      return alert(`You already guessed ${userGuess}!`);
+    }
+    guessedLetters += `${userGuess} `;
 
-// Loop used to display the number of characters in the moviePick with "_" so the player can see how many characters are in the word
-for (var i = 0; i < moviePick.length; i++){
-    wordBeingGuessed += "_ ";
-}
-
-// Function to start once key is released
-document.onkeydown = function game(){
-
-     // Declares variable userGuess as the key pressed and translates to lower case
-     var userGuess = event.key.toLowerCase();
-
-    // Gets rid of the Let's get started HTML prompting the player to hit a key to play
-    document.getElementById("getStarted").innerHTML = " ";
-
-    // Dynamically displays wins using .innerHTML
-    document.getElementById("wins").innerHTML = "Wins: " + wins;
-
-    // Dynamically displays losses using .innerHTML
-    document.getElementById("losses").innerHTML = "Losses: " + losses;
-
-    // Dynamically displays the word that is being guessed (changing "_" to letters)
-    document.getElementById("wordBeingGuessed").innerHTML = wordBeingGuessed;
-
-    // Dynamically displays what letters the player has already guessed
-    document.getElementById("lettersAlreadyGuessed").innerHTML = "You have already guessed: " + guessedLetters;
-
-    // Dynamically displays how many remaining guesses the player has
-    document.getElementById("numberOfGuesses").innerHTML = "You have " + remainingGuesses + " guesses left";
-
-
-    
-
-
-    // Records positions of letters in an array used as reference between letters guessed correctly and their position in moviePick
-    var collectPositions = [];
-
-        // Loop setting i to the length of moviePick
-        for (var i = 0; i < moviePick.length; i++){
-
-            // If the letter the player guessed (userGuess) matched a letter in the movie randomly chosen (moviePickLowerCase) then the number of that character will be recorded by i and pushed into the collectPositions array to be later recalled and substituted in the html for the user to see
-            if (moviePickLowerCase[i] === userGuess){
-            collectPositions.push(i);
-            
-            
-            }
-        }
-
-        wordBeingGuessed = wordBeingGuessed.substr(0, collectPositions[0] * 2) + userGuess + " " + wordBeingGuessed.substr((collectPositions[0] * 2) + 2, wordBeingGuessed.length * 2);
-            console.log (collectPositions);
-
-        // If the letter guessed did not match a letter in the movie generated then that letter will be pushed into the guessedLetters array for the player to see in the html and the player will lose a guess
-        if (collectPositions.length === 0){
-            remainingGuesses--;
-            guessedLetters.push(userGuess.toUpperCase());
-        }
-
-
-        // An else statement that loops through the numbers in the collectPositions array populated by a correct player input that was translated into an index, marking the position of that character. Then the statement takes the letter form userGuess and feeds that into the right index of wordBeingGuessed to later display in the html
-        // else {
-        //     for(var i = 0; i < collectPositions.length; i++) {
-        //         wordBeingGuessed(collectPositions[i]) = userGuess;
-        //     }
-        // }
-    
-    // If there are no more "_" in the wordBeingGuessed (moviePick) it means that all of the letters have been guessed correctly before the player ran out of guesses so the player gets a win
-    if (moviePickLowerCase === wordBeingGuessed.replace((/\s/g), '')) {
-        wins++;
+    if (moviePick.indexOf(userGuess) === -1) {
+      remainingGuesses--;
+      return updateHTML(wordBeingGuessed, guessedLetters, remainingGuesses);
+    } else {
+      let letterIndex = moviePick.indexOf(userGuess);
+      return console.log(`Letter Index ${letterIndex}`);
+      // wordBeingGuessed.replace(/\s/g, '');
     }
 
-    // If remainingGuesses = 0 then the game resets and a new movie is generated for the player to guess
-    if (remainingGuesses == 0) {
-        losses++;
-        gameReset();
-    }
+    updateHTML(wordBeingGuessed, guessedLetters, remainingGuesses);
+  });
+};
 
+const randomMovie = () => {
+  return movieList[Math.floor(Math.random() * movieList.length)];
+};
 
-            console.log(remainingGuesses);
-            console.log(userGuess);
-            console.log(collectPositions);
-            
-}
-    
+const updateHTML = (wordBeingGuessed, guessedLetters, remainingGuesses) => {
+  document.getElementById('get-started').innerHTML = '';
+  document.getElementById('wins').innerHTML = `Wins: ${wins}`;
+  document.getElementById('losses').innerHTML = `Losses: ${losses}`;
+  document.getElementById('word-being-guessed').innerHTML = wordBeingGuessed;
+  document.getElementById(
+    'letters-already-guessed'
+  ).innerHTML = `You have already guessed: ${guessedLetters}`;
+  document.getElementById(
+    'number-of-guesses'
+  ).innerHTML = `You have ${remainingGuesses} guesses left`;
+};
 
+document.addEventListener('keyup', event => {
+  console.log(event);
+  if (event.code === 'Space' && spaceCounter === 0) {
+    spaceCounter++;
+    start();
+  }
+});
